d91835bd008476b50d23ca0f228a997fd5c13195
ItaloCobains <italo.brandao@simpleagro.com.br>
2023-12-18 09:41:31 -0300

    blocked host resolve

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,6 +11,7 @@ module RubyRailsPostgres
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.hosts << 'rails-production-60de.up.railway.app'
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
