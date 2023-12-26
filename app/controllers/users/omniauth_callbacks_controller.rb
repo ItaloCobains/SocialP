09923fa7689576c22a6ddbd565aa089d9db6e444
ItaloCobains <italobrandao.contato@gmail.com>
2023-12-25 21:18:26 -0300

    Remove unnecessary configuration files and update database settings

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,7 +3,6 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    debugger
     sign_in_and_redirect @user
   end
 end
--- config/setup_app.sh
+++ config/setup_app.sh
@@ -4,7 +4,7 @@ set -e
 
 bundle check || bundle install --jobs 5 --retry 5
 
-if [-f tmp/pids/server.pid]; then
+if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
 
